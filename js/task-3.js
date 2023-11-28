32ae92c0ca65352d25e7c426468e714d1ab1fcd2
exl17 <143128264+exl17@users.noreply.github.com>
2023-11-28 22:09:26 +0200

    homework-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,17 @@
-function getElementWidth(content, padding, border) {
-    return Number.parseFloat(content) + 2* (Number.parseFloat(padding)+ Number.parseFloat(border));
+function filterArray(numbers, value) {
+    let newArray = [];
+   for (let i = 0; i < numbers.length; i++) {
+     
+        if (numbers[i] > value) {
+            newArray.push(numbers[i]);
+        }
+    }
+    
+    return newArray;
 }
 
-
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
